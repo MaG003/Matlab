e371d9f258915065521c07d12b81bc0561519bb6
Hoàng Việt <83023633+MaG003@users.noreply.github.com>
2022-05-04 09:32:56 +0700

    Delete matlab9.m

--- matlab9.m
+++
@@ -1,9 +0,0 @@
-% Phương pháp sai phân tính 1 hàm số bất kỳ
-fx = @(x)sin(x);
-A = pi/3;
-Chinh_xac = cos(A)
-for i = 0:6
-    h = 1/10^i;
-    Gan_dung = (feval(fx,A+h) - feval(fx,A-h))/2*h;
-    fprintf('%f\t%f\n',h,Gan_dung);
-end
